f9595215887e2b8283f35deea0355a468ebe2285
CODOmicrobit <50570424+CODOmicrobit@users.noreply.github.com>
2020-01-22 16:32:12 +0100

    Update Grove_Color_Sensor.ts

--- Grove_Color_Sensor.ts
+++ Grove_Color_Sensor.ts
@@ -2,7 +2,7 @@
 // Use the VARIABLE.defl=VALUE notation to specify default argument values.
 //
 
-//% color="#AA278D"
+//% weight=1 color=#004696 icon="\uf121" block="Grove Color Sensor" advanced=false
 namespace basic {
     //% block
     //% x.defl=42
