96d8820ad3e75258bcdff1ec793a1f0eaa510270
CODOmicrobit <50570424+CODOmicrobit@users.noreply.github.com>
2020-01-22 17:10:11 +0100

    Update Grove_Color_Sensor.ts

--- Grove_Color_Sensor.ts
+++ Grove_Color_Sensor.ts
@@ -56,7 +56,7 @@ namespace A4_Robot_Driver {
      */
 
     //% blockId=A4_Robot_Driver_Robot_Move
-    //% block="Grove_Color_Sensor %dir| vitesse %speed"
+    //% block="Groove_Color_Sensor %dir| vitesse %speed"
     //% speed.min=0 speed.max=100
     //% parts="A4_Robot_Driver" advanced=false
     //% speed.defl=75
