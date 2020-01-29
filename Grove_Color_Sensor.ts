cc4322b7121286db2330c45c59eb0885d1ce0136
CODOmicrobit <50570424+CODOmicrobit@users.noreply.github.com>
2020-01-29 18:01:03 +0100

    Update Grove_Color_Sensor.ts

--- Grove_Color_Sensor.ts
+++ Grove_Color_Sensor.ts
@@ -3,19 +3,10 @@
  */
 
 //% weight=1 color=#004696 icon="\uf121" block="Grove_Color_Sensor" advanced=false
-namespace A4_Robot_Driver {
-    /**
-     * Configure la position angulaire d'un servomoteur
-     * @param pin choix du moteur: droit ou gauche
-     */
+namespace Grove_Color_Sensor {
+    //% block
+    export function get_color() {
 
-    //% blockId=A4_Robot_Driver_servo_degrees
-    //% block="positionner le servo sur |%pin| à |%angle| degrés"
-    //% parts="A4_Robot_Driver" advanced=false
-    //% angle.shadow="protractorPicker"
-    //% angle.defl=90
-    export function setServoMotor(pin: AnalogPin, angle: number): void {
-        pins.servoWritePin(pin, Math.constrain(angle, 0, 180));
     }
 
 }
