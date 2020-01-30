6279f1b00b823fad3658d9d8bc6678e868c66566
CODOmicrobit <50570424+CODOmicrobit@users.noreply.github.com>
2020-01-30 13:34:00 +0100

    Update Grove_Color_Sensor.ts

--- Grove_Color_Sensor.ts
+++ Grove_Color_Sensor.ts
@@ -11,7 +11,7 @@ enum Couleur {
 //% weight=1 color=#004696 icon="\uf121" block="Grove_Color_Sensor" advanced=false
 namespace Grove_Color_Sensor {
     //% block
-    export function get_color() {
+    export function get_color(coul: Couleur) {
         pins.i2cWriteNumber( 41, 146, NumberFormat.Int8BE, false)
         let nums: numbernums = pins.i2cReadNumber(41, NumberFormat.UInt8BE, false)
         return nums;
