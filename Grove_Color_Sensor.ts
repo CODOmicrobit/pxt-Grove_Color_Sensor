395be0e65ecc563830e6fcaed6358deb312e89ed
CODOmicrobit <50570424+CODOmicrobit@users.noreply.github.com>
2020-01-30 13:29:36 +0100

    Update Grove_Color_Sensor.ts

--- Grove_Color_Sensor.ts
+++ Grove_Color_Sensor.ts
@@ -1,12 +1,20 @@
 /**
  * Librairie Makecode pour Grove_Color_Sensor.
  */
+enum Couleur {
+    Rouge,
+    Vert,
+    Bleu,
+    Eclairage
+}
 
 //% weight=1 color=#004696 icon="\uf121" block="Grove_Color_Sensor" advanced=false
 namespace Grove_Color_Sensor {
     //% block
     export function get_color() {
-
+        pins.i2cWriteNumber( 41, 146, NumberFormat.Int8BE, false)
+        let nums: numbernums = pins.i2cReadNumber(41, NumberFormat.UInt8BE, false)
+        return nums;
     }
 
 }
